ab4dab4ee6fcdf69d25ab973bbd6e7fee8ed1664
g00414253 <g00414253@atu.ie>
2023-10-31 20:45:21 +0000

    Created Default constructor,Constructor and created a function to display an instance of the object

--- src/main/java/ie/StudentApp/main/Main.java
+++ src/main/java/ie/StudentApp/main/Main.java
@@ -6,10 +6,31 @@ package ie.StudentApp.main;
     String firstName,lastName;
     long Course,email;
 
+//Constructor
+     public Student(String firstName,String lastName,long Course,long email){
+         this.firstName=firstName;
+         this.lastName=lastName;
+         this.Course=Course;
+         this.email=email;
+     }
+
+     //Default Constructor
+     public Student(){
+         this.firstName=Evan;
+         this.lastName=Duffy;
+         this.Course=Software;
+         this.email=g00414253;
+     }
+
+
 
 public void displayInfo(){
     System.out.println("Name: " + firstName "" + lastName ", Email: " + email ", Course: " + Course);
 }
 
 
+}
+
+
+public class Main {
 }
